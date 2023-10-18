1b6f9e7faf9c819c1b470fd202d427b6a015f052
tadiwa <pfachitadiwanashedavid@gmail.com>
2023-10-18 16:08:49 +0200

    authentication

--- src/App.js
+++ src/App.js
@@ -2,23 +2,64 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home";
 import Jobs from "./pages/jobs";
+import PostJob from "./pages/postJobs";
 import About from "./pages/about";
+import Login from "./pages/login";
 import Error from "./pages/errors";
+import { useState } from "react";
 
 function App() {
+  const [isAuth, setIsAuth] = useState("false"); 
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/jobs">Jobs</Link>
-        <Link to="/about">About</Link>
+            <nav>
+        <ul className="navLinks">
+          <li>
+            <Link to="/" className="link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/jobs" className="link">
+              Explore Jobs
+            </Link>
+          </li>
+          <li>
+            <Link to="/postJobs" className="link">
+              Post Jobs
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="link">
+              About Us
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <h3 className="logo">mukunda.</h3>
+        </div>
+        <button className="logInbtn">
+          {! isAuth ?<Link to="/login" className="link">
+            log in
+          </Link>: "Tadiwa"}
+        </button>
+        <button className="signupBtn">
+          {! isAuth ? <Link to="/login" className="link">
+            Sign up
+          </Link> : <Link to="/logout" className="link">
+            Sign out
+          </Link>}
+        </button>
       </nav>
       <Routes>
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/about" element = { <About/> } />
-        <Route path = "/jobs" element = { <Jobs/> } />
-        <Route path = "*" element = { <Error/> } />
-        </Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about Us" element={<About />} />
+        <Route path="/postJobs" element={<PostJob />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <footer>mukundazw 2023</footer>
     </Router>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,19 @@
 import React from "react";
+// import {styles} from "./home.module.css";
+
+const bgPicture = new URL("../images/bg.jpg",import.meta.url);
 
 function home() {
   return (
-    <div>
-      <h1>This is home page</h1>
-    </div>
+    <div /*className={styles.homepageBackground}*/>
+        <p>
+            <a
+                href="https://www.freepik.com/free-photo/3d-man-stood-top-mountain-against-space-sky_2999966.htm#query=blue%20sky%20out%20of%20space&position=7&from_view=search&track=ais">
+                Image by kjpargeter
+            </a>
+            on Freepik
+        </p> 
+    </div> 
   );
 }
 
