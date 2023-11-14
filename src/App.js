f0a3b869d0555317aa25ba47996c3ca4dc954f4f
tadiwa <pfachitadiwanashedavid@gmail.com>
2023-11-14 18:15:06 +0200

    update

--- src/App.js
+++ src/App.js
@@ -9,57 +9,69 @@ import Error from "./pages/errors";
 import { useState } from "react";
 
 function App() {
-  const [isAuth, setIsAuth] = useState("false"); 
+  const [isAuth, setIsAuth] = useState("false");
   return (
     <Router>
-            <nav>
-        <ul className="navLinks">
-          <li>
-            <Link to="/" className="link">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/jobs" className="link">
-              Explore Jobs
-            </Link>
-          </li>
-          <li>
-            <Link to="/postJobs" className="link">
-              Post Jobs
-            </Link>
-          </li>
-          <li>
-            <Link to="/about" className="link">
-              About Us
-            </Link>
-          </li>
-        </ul>
-        <div>
+      <header className="headerWrapper">
+        <div className="navWrapper">
+          <ul className="navLinks">
+            <li>
+              <Link to="/" className="link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/jobs" className="link">
+                Explore Jobs
+              </Link>
+            </li>
+            <li>
+              <Link to="/postJobs" className="link">
+                Post Jobs
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="link">
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="logoWrapper">
           <h3 className="logo">mukunda.</h3>
         </div>
-        <button className="logInbtn">
-          {! isAuth ?<Link to="/login" className="link">
-            log in
-          </Link>: "Tadiwa"}
-        </button>
-        <button className="signupBtn">
-          {! isAuth ? <Link to="/login" className="link">
-            Sign up
-          </Link> : <Link to="/logout" className="link">
-            Sign out
-          </Link>}
-        </button>
-      </nav>
+        <div className="buttonWrapper">
+          <button className="logInbtn">
+            {!isAuth ? (
+              <Link to="/login" className="link">
+                log in
+              </Link>
+            ) : (
+              "Tadiwa"
+            )}
+          </button>
+          <button className="signupBtn">
+            {!isAuth ? (
+              <Link to="/login" className="link">
+                Sign up
+              </Link>
+            ) : (
+              <Link to="/logout" className="link">
+                Sign out
+              </Link>
+            )}
+          </button>
+        </div>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about Us" element={<About />} />
         <Route path="/postJobs" element={<PostJob />} />
         <Route path="/jobs" element={<Jobs />} />
-        <Route path="/login" element={<Login setIsAuth={setIsAuth}/>} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      <footer>mukundazw 2023</footer>
+      {/* <footer><code>mukundazw 2023</code></footer> */}
     </Router>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,20 +1,70 @@
-import React from "react";
-// import {styles} from "./home.module.css";
+import React, { useState, useEffect } from "react";
+import styles from "./home.module.css";
 
-const bgPicture = new URL("../images/bg.jpg",import.meta.url);
+function Home() {
 
-function home() {
+  const [quote, setQuote] = useState("");
+
+  //run the api call after 1 minute
+  setInterval(setQuote, 60 * 1000);
+
+  useEffect(()=>{ 
+    setQuote((quote) => {
+
+    // get quote index number from random number generator
+    function getRandomNumber(min, max) {
+      return Math.floor(Math.random()) * (max - min) + min;
+    };
+    let index = getRandomNumber(0, 15);
+
+    // fetch quotes for api
+    if (index <= 15){quote = (() => {
+      fetch("https://type.fit/api/quotes")
+        .then((response) => {
+          return response.json()
+        })
+        .then((data) => { console.log(data[index]);
+          return data[index] 
+          }
+        )
+        return quote
+    });}
+    else { 
+      return quote =  "No quotes find error"
+     };
+
+
+  });
+}, [])
+
+
   return (
-    <div /*className={styles.homepageBackground}*/>
-        <p>
-            <a
-                href="https://www.freepik.com/free-photo/3d-man-stood-top-mountain-against-space-sky_2999966.htm#query=blue%20sky%20out%20of%20space&position=7&from_view=search&track=ais">
-                Image by kjpargeter
-            </a>
-            on Freepik
-        </p> 
-    </div> 
+    <div className={styles.homepageBackground}>
+      <div className={styles.motivationalQuoteWrapper}>
+        <p className={styles.quote}>
+          {quote}
+        </p>
+      </div>
+      {/* <div className={styles.mukundaMottoWrapper}>
+        <p className={styles.motto}>
+          Mukunda is a one-stop-center for all freelance and full-time Tech jobs
+          in the country
+        </p>
+      </div> */}
+      <div className={styles.searchBarWrapper}>
+        <input type="search" placeholder="search here" className={styles.jobtype} />
+        <select className={styles.jobLocation}>
+          <option value="">select location</option>
+          <option value="">Harare</option>
+          <option value="">Mutare</option>
+          <option value="">Bulawayo</option>
+          <option value="">Rusape</option>
+          <option value="">Victoria Falls</option>
+        </select>
+        <button type="submit" className={styles.jobSearch}>Search</button>
+      </div>
+    </div>
   );
 }
 
-export default home;
+export default Home;
